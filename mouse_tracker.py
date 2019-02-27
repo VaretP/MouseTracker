76cd143e9a35d42446ee80305f0071f430501d44
Paul Varet <paul.varet@epita.fr>
2019-02-27 22:01:40 +0100

    [SAVE] save number of meters travelled, and load save at launch

--- mouse_tracker.py
+++ mouse_tracker.py
@@ -3,14 +3,21 @@
 from Xlib import display
 from math import sqrt
 from subprocess import call
+import os.path
 
-def tracker():
+
+def save_meters(meters):
+    save = open(os.path.expanduser('~/.mouse_tracking'), 'w')
+    save.write(str(meters))
+    save.close()
+
+
+def tracker(meters):
     disp = display.Display().screen()
     conv = (disp.width_in_mms / 10) / disp.width_in_pixels
     travelled = 0
     pos = disp.root.query_pointer()._data
     last_x, last_y = pos['root_x'], pos['root_y']
-    meters = 0
     while 1:
         pos = disp.root.query_pointer()._data
         if pos['root_x'] != last_x and pos['root_y'] != last_y:
@@ -20,12 +27,24 @@ def tracker():
             if travelled >= 100:
                 travelled -= 100
                 meters += 1
+                save_meters(meters)
                 if meters == 1:
                     call(['notify-send', 'Mouse Tracker', '1m !'])
                 if not meters % 1000:
                     call(['notify-send', 'Mouse Tracker',
                         str(meters / 1000) + 'km !'])
-            
+    
+
+def read_save():
+    try:
+        save = open(os.path.expanduser('~/.mouse_tracking'), 'r')
+        val = int(save.read())
+    except:
+        save = open(os.path.expanduser('~/.mouse_tracking'), 'w+')
+        val = 0
+    save.close()
+    return val
+
 
 if __name__ == '__main__':
-    tracker()
+    tracker(read_save())
